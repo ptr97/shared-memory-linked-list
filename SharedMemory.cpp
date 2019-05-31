8c2fa30a1c0b2999a866d1e66b620b251e4f64e6
Piotr <ptr.wos@gmail.com>
2019-05-31 20:23:13 +0200

    Move shmBlock functions to cpp file

--- SharedMemory.cpp
+++ SharedMemory.cpp
@@ -1,3 +1,53 @@
 #include "SharedMemory.h"
 
 char * shmBlock::startPtr;
+
+void shmBlock::allocateMemory(const char * key, uint size) {
+  const int fd = open(key, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+  if( fd < 0 ) {
+    std::cout << "Open DB error" << std::endl;
+    exit(-1);
+  }
+
+  const int status = ftruncate(fd, static_cast<off_t>(size));
+  if(status != 0) {
+    std::cout << "ftrucate error" << std::endl;
+    exit(-1);
+  }
+
+  shmBlock::startPtr = (char *) mmap(0, size, PROT_WRITE, MAP_SHARED, fd, 0);
+  if(shmBlock::startPtr == MAP_FAILED) {
+    std::cout << "mmap failed" << std::endl;
+    exit(-1);
+  }
+
+  memset(shmBlock::startPtr, '\0', static_cast<off_t>(size));
+  std::cout << "Memory block of size " << size << " allocated at: " << (void *) shmBlock::startPtr << std::endl;
+}
+
+uint shmBlock::readFromMemory(const char * key) {
+  const int fd = open(key, O_RDONLY, S_IRUSR | S_IWUSR);
+  if(fd < 0) {
+    std::cout << "Open DB error" << std::endl;
+    exit(-1);
+  }
+
+  struct stat file_statistics;
+  fstat(fd, &file_statistics);
+
+  shmBlock::startPtr = (char *) mmap(0, file_statistics.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if(shmBlock::startPtr == MAP_FAILED) {
+    std::cout << "mmap failed" << std::endl;
+    exit(-1);
+  }
+  std::cout << "Memory block of size " << file_statistics.st_size << " read at: " << (void *) shmBlock::startPtr << std::endl;
+  return file_statistics.st_size;
+}
+
+void shmBlock::freeShm(uint size) {
+  if(munmap(shmBlock::startPtr, static_cast<off_t>(size))) {
+    std::cout << "unmap error" << std::endl;
+    exit(-1);
+  }
+  std::cout << "Memory block has been released..." << std::endl;
+}
--- SharedMemory.h
+++ SharedMemory.h
@@ -16,56 +16,9 @@ class shmBlock
 public:
   static char * startPtr;
 
-  static void allocateMemory(const char * key, uint size) {
-    const int fd = open(key, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if( fd < 0 ) {
-      std::cout << "Open DB error" << std::endl;
-      exit(-1);
-    }
-
-    const int status = ftruncate(fd, static_cast<off_t>(size));
-    if(status != 0) {
-      std::cout << "ftrucate error" << std::endl;
-      exit(-1);
-    }
-
-    shmBlock::startPtr = (char *) mmap(0, size, PROT_WRITE, MAP_SHARED, fd, 0);
-    if(shmBlock::startPtr == MAP_FAILED) {
-      std::cout << "mmap failed" << std::endl;
-      exit(-1);
-	  }
-
-    memset(shmBlock::startPtr, '\0', static_cast<off_t>(size));
-    std::cout << "Memory block of size " << size << " allocated at: " << (void *) shmBlock::startPtr << std::endl;
-  }
-
-  static void readFromMemory(const char * key) {
-    const int fd = open(key, O_RDONLY, S_IRUSR | S_IWUSR);
-    if(fd < 0) {
-      std::cout << "Open DB error" << std::endl;
-      exit(-1);
-    }
-
-    struct stat file_statistics;
-    fstat(fd, &file_statistics);
-
-    shmBlock::startPtr = (char *) mmap(0, file_statistics.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if(shmBlock::startPtr == MAP_FAILED) {
-      std::cout << "mmap failed" << std::endl;
-      exit(-1);
-	  }
-
-    std::cout << "Memory block of size " << file_statistics.st_size << " read at: " << (void *) shmBlock::startPtr << std::endl;
-  }
-
-  static void freeShm(uint size) {
-    if(munmap(shmBlock::startPtr, static_cast<off_t>(size))) {
-      std::cout << "unmap error" << std::endl;
-      exit(-1);
-	  }
-
-    std::cout << "Memory block has been released..." << std::endl;
-  }
+  static void allocateMemory(const char * key, uint size);
+  static uint readFromMemory(const char * key);
+  static void freeShm(uint size);
 };
 
 #endif
